41e6ef11f762562c4564e87d8915323c1b7a4001
Jameson Molnar <JMolnar@zscaler.com>
2022-07-16 23:05:14 -0400

    more zsec functionality additions

--- examples/base/output.tf
+++ examples/base/output.tf
@@ -2,7 +2,8 @@ locals {
 
   testbedconfig = <<TB
 
-1) Copy the SSH key to the bastion host
+1) Change to deployment base directory and copy the SSH key to the bastion host
+cd base
 scp -i ${var.name_prefix}-key-${random_string.suffix.result}.pem ${var.name_prefix}-key-${random_string.suffix.result}.pem ubuntu@${module.bastion.public_ip}:/home/${var.server_admin_username}/.
 
 2) SSH to the bastion host
