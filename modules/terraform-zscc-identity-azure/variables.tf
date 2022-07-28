a1f471a423cb71b0d3f71d70da8bd14f6a81fd44
Jameson Molnar <JMolnar@zscaler.com>
2022-07-27 21:30:47 -0400

    identity modules readme + cleanup

--- modules/terraform-zscc-identity-azure/variables.tf
+++ modules/terraform-zscc-identity-azure/variables.tf
@@ -1,9 +1,10 @@
 variable "cc_vm_managed_identity_name" {
-  description = "Azure Managed Identity name to attach to the CC VM. E.g zspreview-66117-mi"
   type        = string
+  description = "Azure Managed Identity name to attach to the CC VM. E.g zspreview-66117-mi"
+
 }
 
 variable "cc_vm_managed_identity_rg" {
-  description = "Resource Group of the Azure Managed Identity name to attach to the CC VM. E.g. edgeconnector_rg_1"
   type        = string
+  description = "Resource Group of the Azure Managed Identity name to attach to the CC VM. E.g. edgeconnector_rg_1"
 }
