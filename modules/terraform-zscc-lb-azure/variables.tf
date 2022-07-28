368a569b1fa1f18cf9c7e1c0db431538fa8fe9bc
Jameson Molnar <JMolnar@zscaler.com>
2022-07-27 21:26:31 -0400

    lb modules readme + cleanup

--- modules/terraform-zscc-lb-azure/variables.tf
+++ modules/terraform-zscc-lb-azure/variables.tf
@@ -1,37 +1,47 @@
 variable "name_prefix" {
-  description = "A prefix to associate to all the module resources"
-  default     = "zs"
+  type        = string
+  description = "A prefix to associate to all the lb module resources"
+  default     = null
 }
 
 variable "resource_tag" {
-  description = "A tag to associate to all the module resources"
-  default     = "cloud-connector"
+  type        = string
+  description = "A tag to associate to all the lb module resources"
+  default     = null
+}
+
+variable "global_tags" {
+  type        = map(string)
+  description = "Populate any custom user defined tags from a map"
+  default     = {}
 }
 
 variable "resource_group" {
+  type        = string
   description = "Main Resource Group Name"
 }
 
+variable "location" {
+  type        = string
+  description = "Cloud Connector Azure Region"
+}
+
 variable "subnet_id" {
+  type        = string
   description = "Subnet ID for LB Frontend IP placement"
 }
 
-
 variable "http_probe_port" {
+  type        = number
   description = "port for Cloud Connector cloud init to enable listener port for HTTP probe from LB"
-  default     = 0
+  default     = 50000
   validation {
     condition = (
-      var.http_probe_port == 0 ||
       var.http_probe_port == 80 ||
       (var.http_probe_port >= 1024 && var.http_probe_port <= 65535)
     )
     error_message = "Input http_probe_port must be set to a single value of 80 or any number between 1024-65535."
   }
-}
-
-variable "location" {
-  description = "Azure Region"
 }
 
 variable "load_distribution" {
@@ -47,7 +57,3 @@ variable "load_distribution" {
     error_message = "Input load_distribution must be set to either SourceIP, SourceIPProtocol, or Default."
   }
 }
-variable "global_tags" {
-  description = "populate custom user provided tags"
-}
-
