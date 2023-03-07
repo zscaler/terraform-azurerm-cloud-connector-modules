392d488ec5e7cf2420f33579bb798f33232f4b23
Jameson Molnar <JMolnar@zscaler.com>
2023-03-07 14:02:12 -0500

    fix: nsg data source read logic

--- modules/terraform-zscc-nsg-azure/main.tf
+++ modules/terraform-zscc-nsg-azure/main.tf
@@ -48,8 +48,8 @@ resource "azurerm_network_security_group" "cc_mgmt_nsg" {
 
 # Or use existing Mgmt NSG
 data "azurerm_network_security_group" "mgt_nsg_selected" {
-  count               = var.byo_nsg == false ? length(azurerm_network_security_group.cc_mgmt_nsg[*].id) : length(var.byo_mgmt_nsg_names)
-  name                = var.byo_nsg == false ? "${var.name_prefix}-cc-mgmt-nsg-${count.index + 1}-${var.resource_tag}" : element(var.byo_mgmt_nsg_names, count.index)
+  count               = var.byo_nsg ? length(var.byo_mgmt_nsg_names) : 0
+  name                = var.byo_nsg ? element(var.byo_mgmt_nsg_names, count.index) : 0
   resource_group_name = var.resource_group
 }
 
@@ -92,7 +92,7 @@ resource "azurerm_network_security_group" "cc_service_nsg" {
 
 # Or use existing Service NSG
 data "azurerm_network_security_group" "service_nsg_selected" {
-  count               = var.byo_nsg == false ? length(azurerm_network_security_group.cc_service_nsg[*].id) : length(var.byo_service_nsg_names)
-  name                = var.byo_nsg == false ? "${var.name_prefix}-cc-service-nsg-${count.index + 1}-${var.resource_tag}" : element(var.byo_service_nsg_names, count.index)
+  count               = var.byo_nsg ? length(var.byo_service_nsg_names) : 0
+  name                = var.byo_nsg ? element(var.byo_service_nsg_names, count.index) : 0
   resource_group_name = var.resource_group
 }
--- modules/terraform-zscc-nsg-azure/outputs.tf
+++ modules/terraform-zscc-nsg-azure/outputs.tf
@@ -1,9 +1,9 @@
 output "mgmt_nsg_id" {
   description = "Management Network Security Group ID"
-  value       = data.azurerm_network_security_group.mgt_nsg_selected[*].id
+  value       = var.byo_nsg ? data.azurerm_network_security_group.mgt_nsg_selected[*].id : azurerm_network_security_group.cc_mgmt_nsg[*].id
 }
 
 output "service_nsg_id" {
   description = "Service Network Security Group ID"
-  value       = data.azurerm_network_security_group.service_nsg_selected[*].id
+  value       = var.byo_nsg ? data.azurerm_network_security_group.service_nsg_selected[*].id : azurerm_network_security_group.cc_service_nsg[*].id
 }
