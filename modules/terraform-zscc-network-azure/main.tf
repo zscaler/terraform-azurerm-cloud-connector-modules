71caffc1b6ab9fa95f90993f4215988c19f046d8
Jameson Molnar <JMolnar@zscaler.com>
2023-03-31 13:58:01 -0400

    refactor: terraform-zscc-network-azure for private dns support

--- modules/terraform-zscc-network-azure/main.tf
+++ modules/terraform-zscc-network-azure/main.tf
@@ -16,7 +16,8 @@ resource "azurerm_resource_group" "rg" {
 }
 
 data "azurerm_resource_group" "rg_selected" {
-  name = var.byo_rg == false ? azurerm_resource_group.rg[0].name : var.byo_rg_name
+  count = var.byo_rg ? 1 : 0
+  name  = var.byo_rg_name
 }
 
 
@@ -29,14 +30,15 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "${var.name_prefix}-vnet-${var.resource_tag}"
   address_space       = [var.network_address_space]
   location            = var.location
-  resource_group_name = data.azurerm_resource_group.rg_selected.name
+  resource_group_name = try(data.azurerm_resource_group.rg_selected[0].name, azurerm_resource_group.rg[0].name)
 
   tags = var.global_tags
 }
 
 data "azurerm_virtual_network" "vnet_selected" {
-  name                = var.byo_vnet == false ? azurerm_virtual_network.vnet[0].name : var.byo_vnet_name
-  resource_group_name = var.byo_vnet == false ? azurerm_virtual_network.vnet[0].resource_group_name : var.byo_vnet_subnets_rg_name
+  count               = var.byo_vnet ? 1 : 0
+  name                = var.byo_vnet_name
+  resource_group_name = var.byo_vnet_subnets_rg_name
 }
 
 
@@ -48,7 +50,7 @@ resource "azurerm_public_ip" "pip" {
   count                   = var.byo_pips == false && var.base_only == false ? length(distinct(var.zones)) : 0
   name                    = "${var.name_prefix}-public-ip-${count.index + 1}-${var.resource_tag}"
   location                = var.location
-  resource_group_name     = data.azurerm_resource_group.rg_selected.name
+  resource_group_name     = try(data.azurerm_resource_group.rg_selected[0].name, azurerm_resource_group.rg[0].name)
   allocation_method       = "Static"
   sku                     = "Standard"
   idle_timeout_in_minutes = 30
@@ -62,9 +64,9 @@ resource "azurerm_public_ip" "pip" {
 }
 
 data "azurerm_public_ip" "pip_selected" {
-  count               = var.byo_pips == false ? length(azurerm_public_ip.pip[*].id) : length(var.byo_pip_names)
-  name                = var.byo_pips == false ? azurerm_public_ip.pip[count.index].name : element(var.byo_pip_names, count.index)
-  resource_group_name = var.byo_pips == false ? data.azurerm_resource_group.rg_selected.name : var.byo_pip_rg
+  count               = var.byo_pips ? length(var.byo_pip_names) : 0
+  name                = element(var.byo_pip_names, count.index)
+  resource_group_name = var.byo_pip_rg
 }
 
 
@@ -73,7 +75,7 @@ resource "azurerm_nat_gateway" "ngw" {
   count                   = var.byo_nat_gws == false && var.base_only == false ? length(distinct(var.zones)) : 0
   name                    = "${var.name_prefix}-ngw-${count.index + 1}-${var.resource_tag}"
   location                = var.location
-  resource_group_name     = data.azurerm_resource_group.rg_selected.name
+  resource_group_name     = try(data.azurerm_resource_group.rg_selected[0].name, azurerm_resource_group.rg[0].name)
   idle_timeout_in_minutes = 10
   zones                   = local.zones_supported ? [element(var.zones, count.index)] : null
 
@@ -81,21 +83,16 @@ resource "azurerm_nat_gateway" "ngw" {
 }
 
 data "azurerm_nat_gateway" "ngw_selected" {
-  count               = var.byo_nat_gws == false ? length(azurerm_nat_gateway.ngw[*].id) : length(var.byo_nat_gw_names)
-  name                = var.byo_nat_gws == false ? azurerm_nat_gateway.ngw[count.index].name : element(var.byo_nat_gw_names, count.index)
-  resource_group_name = var.byo_nat_gws == false ? data.azurerm_resource_group.rg_selected.name : var.byo_nat_gw_rg
+  count               = var.byo_nat_gws ? length(var.byo_nat_gw_names) : 0
+  name                = element(var.byo_nat_gw_names, count.index)
+  resource_group_name = var.byo_nat_gw_rg
 }
 
 # Associate Public IP to NAT Gateway
 resource "azurerm_nat_gateway_public_ip_association" "ngw_association" {
-  count                = var.existing_nat_gw_pip_association == false ? length(data.azurerm_nat_gateway.ngw_selected[*].id) : 0
-  nat_gateway_id       = data.azurerm_nat_gateway.ngw_selected[count.index].id
-  public_ip_address_id = data.azurerm_public_ip.pip_selected[count.index].id
-
-  depends_on = [
-    data.azurerm_public_ip.pip_selected,
-    data.azurerm_nat_gateway.ngw_selected
-  ]
+  count                = var.existing_nat_gw_pip_association == false ? try(length(data.azurerm_nat_gateway.ngw_selected[*].id), length(azurerm_nat_gateway.ngw[*].id)) : 0
+  nat_gateway_id       = try(data.azurerm_nat_gateway.ngw_selected[count.index].id, azurerm_nat_gateway.ngw[count.index].id)
+  public_ip_address_id = try(data.azurerm_public_ip.pip_selected[count.index].id, azurerm_public_ip.pip[count.index].id)
 }
 
 
@@ -106,8 +103,8 @@ resource "azurerm_nat_gateway_public_ip_association" "ngw_association" {
 resource "azurerm_subnet" "cc_subnet" {
   count                = var.byo_subnets == false && var.base_only == false ? length(distinct(var.zones)) : 0
   name                 = "${var.name_prefix}-cc-subnet-${count.index + 1}-${var.resource_tag}"
-  resource_group_name  = var.byo_vnet == false ? data.azurerm_virtual_network.vnet_selected.resource_group_name : var.byo_vnet_subnets_rg_name
-  virtual_network_name = var.byo_vnet == false ? data.azurerm_virtual_network.vnet_selected.name : var.byo_vnet_name
+  resource_group_name  = var.byo_vnet == false ? try(data.azurerm_virtual_network.vnet_selected[0].resource_group_name, azurerm_virtual_network.vnet[0].resource_group_name) : var.byo_vnet_subnets_rg_name
+  virtual_network_name = var.byo_vnet == false ? try(data.azurerm_virtual_network.vnet_selected[0].name, azurerm_virtual_network.vnet[0].name) : var.byo_vnet_name
   address_prefixes     = var.cc_subnets != null ? [element(var.cc_subnets, count.index)] : [cidrsubnet(var.network_address_space, 8, count.index + 200)]
 }
 
@@ -115,19 +112,18 @@ resource "azurerm_subnet" "cc_subnet" {
 data "azurerm_subnet" "cc_subnet_selected" {
   count                = var.byo_subnets == false ? length(azurerm_subnet.cc_subnet[*].id) : length(var.byo_subnet_names)
   name                 = var.byo_subnets == false ? azurerm_subnet.cc_subnet[count.index].name : element(var.byo_subnet_names, count.index)
-  resource_group_name  = var.byo_vnet == false ? data.azurerm_virtual_network.vnet_selected.resource_group_name : var.byo_vnet_subnets_rg_name
-  virtual_network_name = var.byo_vnet == false ? data.azurerm_virtual_network.vnet_selected.name : var.byo_vnet_name
+  resource_group_name  = var.byo_vnet == false ? try(data.azurerm_virtual_network.vnet_selected[0].resource_group_name, azurerm_virtual_network.vnet[0].resource_group_name) : var.byo_vnet_subnets_rg_name
+  virtual_network_name = var.byo_vnet == false ? try(data.azurerm_virtual_network.vnet_selected[0].name, azurerm_virtual_network.vnet[0].name) : var.byo_vnet_name
 }
 
 # Associate Cloud Connector Subnet to NAT Gateway
 resource "azurerm_subnet_nat_gateway_association" "cc_subnet_nat_association" {
   count          = var.existing_nat_gw_subnet_association == false ? length(data.azurerm_subnet.cc_subnet_selected[*].id) : 0
   subnet_id      = data.azurerm_subnet.cc_subnet_selected[count.index].id
-  nat_gateway_id = data.azurerm_nat_gateway.ngw_selected[count.index].id
+  nat_gateway_id = try(data.azurerm_nat_gateway.ngw_selected[count.index].id, azurerm_nat_gateway.ngw[count.index].id)
 
   depends_on = [
     data.azurerm_subnet.cc_subnet_selected,
-    data.azurerm_nat_gateway.ngw_selected
   ]
 }
 
@@ -139,8 +135,8 @@ resource "azurerm_subnet_nat_gateway_association" "cc_subnet_nat_association" {
 resource "azurerm_subnet" "workload_subnet" {
   count                = var.workloads_enabled == true ? 1 : 0
   name                 = "${var.name_prefix}-workload-subnet-${var.resource_tag}"
-  resource_group_name  = data.azurerm_resource_group.rg_selected.name
-  virtual_network_name = data.azurerm_virtual_network.vnet_selected.name
+  resource_group_name  = try(data.azurerm_resource_group.rg_selected[0].name, azurerm_resource_group.rg[0].name)
+  virtual_network_name = try(data.azurerm_virtual_network.vnet_selected[0].name, azurerm_virtual_network.vnet[0].name)
   address_prefixes     = var.workloads_subnets != null ? [element(var.workloads_subnets, count.index)] : [cidrsubnet(var.network_address_space, 8, count.index + 1)]
 }
 
@@ -149,7 +145,7 @@ resource "azurerm_route_table" "workload_rt" {
   count               = var.workloads_enabled == true ? 1 : 0
   name                = "${var.name_prefix}-workload-rt-${var.resource_tag}"
   location            = var.location
-  resource_group_name = data.azurerm_resource_group.rg_selected.name
+  resource_group_name = try(data.azurerm_resource_group.rg_selected[0].name, azurerm_resource_group.rg[0].name)
 
   disable_bgp_route_propagation = true
 
@@ -176,7 +172,52 @@ resource "azurerm_subnet_route_table_association" "workload_rt_association" {
 resource "azurerm_subnet" "bastion_subnet" {
   count                = var.bastion_enabled == true ? 1 : 0
   name                 = "${var.name_prefix}-bastion-subnet-${var.resource_tag}"
-  resource_group_name  = data.azurerm_resource_group.rg_selected.name
-  virtual_network_name = data.azurerm_virtual_network.vnet_selected.name
+  resource_group_name  = try(data.azurerm_resource_group.rg_selected[0].name, azurerm_resource_group.rg[0].name)
+  virtual_network_name = try(data.azurerm_virtual_network.vnet_selected[0].name, azurerm_virtual_network.vnet[0].name)
   address_prefixes     = var.public_subnets != null ? [element(var.public_subnets, count.index)] : [cidrsubnet(var.network_address_space, 8, 101)]
+}
+
+
+################################################################################
+# Outbound Private DNS Subnet and Route Table
+################################################################################
+# Create private subnet for outbound private DNS and delegate to dnsResolvers service
+resource "azurerm_subnet" "private_dns_subnet" {
+  count                = var.zpa_enabled == true ? 1 : 0
+  name                 = "${var.name_prefix}-outbound-dns-subnet-${var.resource_tag}"
+  resource_group_name  = try(data.azurerm_resource_group.rg_selected[0].name, azurerm_resource_group.rg[0].name)
+  virtual_network_name = try(data.azurerm_virtual_network.vnet_selected[0].name, azurerm_virtual_network.vnet[0].name)
+  address_prefixes     = var.private_dns_subnet != null ? [var.private_dns_subnet] : [cidrsubnet(var.network_address_space, 12, 2480)]
+
+  delegation {
+    name = "Microsoft.Network.dnsResolvers"
+    service_delegation {
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+      name    = "Microsoft.Network/dnsResolvers"
+    }
+  }
+}
+
+# Create Outbound DNS Route Table to send to Cloud Connector
+resource "azurerm_route_table" "private_dns_rt" {
+  count               = var.zpa_enabled == true ? 1 : 0
+  name                = "${var.name_prefix}-outbound-dns-rt-${var.resource_tag}"
+  location            = var.location
+  resource_group_name = try(data.azurerm_resource_group.rg_selected[0].name, azurerm_resource_group.rg[0].name)
+
+  disable_bgp_route_propagation = true
+
+  route {
+    name                   = "default-route"
+    address_prefix         = "0.0.0.0/0"
+    next_hop_type          = "VirtualAppliance"
+    next_hop_in_ip_address = var.lb_enabled == true ? var.lb_frontend_ip : element(var.cc_service_ip, count.index)
+  }
+}
+
+# Associate Route Table with Outbound DNS Subnet
+resource "azurerm_subnet_route_table_association" "private_dns_rt_association" {
+  count          = length(azurerm_route_table.private_dns_rt[*].id)
+  subnet_id      = azurerm_subnet.private_dns_subnet[count.index].id
+  route_table_id = azurerm_route_table.private_dns_rt[count.index].id
 }
