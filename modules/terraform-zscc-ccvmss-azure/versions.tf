234c156d251d410e6ed840b64458edc957b15d53
rhartnett-zscaler <rhartnett@zscaler.com>
2023-12-08 10:50:30 -0800

    feat: added module and example for bringing up vmss of cloud connectors. This is not finalized for moving into devel branch but adding it so others can leverage the module for their development

---
+++ modules/terraform-zscc-ccvmss-azure/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.46, <= 3.74"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.1.0"
+    }
+  }
+  required_version = ">= 0.13.7, < 2.0.0"
+}
