263902eeeecf26dbd7defa4ca2ff85f3bdaf2781
Jameson Molnar <JMolnar@zscaler.com>
2024-03-22 17:30:53 -0400

    refactor: split vmss and function resources into separate modules
    * Add zonal_vmss_enabled variable

--- modules/terraform-zscc-ccvmss-azure/outputs.tf
+++ modules/terraform-zscc-ccvmss-azure/outputs.tf
@@ -1,1 +1,9 @@
+output "vmss_names" {
+  description = "VMSS Names"
+  value       = azurerm_orchestrated_virtual_machine_scale_set.cc_vmss[*].name
+}
 
+output "vmss_ids" {
+  description = "VMSS IDs"
+  value       = azurerm_orchestrated_virtual_machine_scale_set.cc_vmss[*].id
+}
