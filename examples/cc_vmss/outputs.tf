4dcb688aac01b79699d6f41153e5e61a5a766592
Jameson Molnar <JMolnar@zscaler.com>
2024-04-03 20:43:58 -0400

    feat: add cc_vmss dtype

---
+++ examples/cc_vmss/outputs.tf
@@ -0,0 +1,60 @@
+locals {
+
+  testbedconfig = <<TB
+***Disclaimer***
+By default, these templates store two critical files to the "examples" directory. DO NOT delete/lose these files:
+1. Terraform State file (terraform.tfstate) - Terraform must store state about your managed infrastructure and configuration. 
+   This state is used by Terraform to map real world resources to your configuration, keep track of metadata, and to improve performance for large infrastructures.
+
+   Terraform uses state to determine which changes to make to your infrastructure. 
+   Prior to any operation, Terraform does a refresh to update the state with the real infrastructure.
+
+   If this file is missing, you will NOT be able to make incremental changes to the environment resources without first importing state back to terraform manually.
+
+2. SSH Private Key (.pem) file - Zscaler templates will attempt to create a new local private/public key pair for VM access (if a pre-existing one is not specified). 
+   You (and subsequently Zscaler) will NOT be able to remotely access these VMs once deployed without valid SSH access.
+***Disclaimer***
+
+
+Resource Group: 
+${module.network.resource_group_name}
+
+LB IP: 
+${module.cc_lb.lb_ip}
+
+VMSS Names:
+${join("\n", module.cc_vmss.vmss_names)}
+
+VMSS IDs:
+${join("\n", module.cc_vmss.vmss_ids)}
+
+Function App ID:
+${module.cc_functionapp.function_app_id}
+
+Function App Outbound IPs:
+${join("\n", module.cc_functionapp.function_app_outbound_ip_address_list)}
+
+All NAT GW IPs:
+${join("\n", module.network.public_ip_address)}
+
+Private DNS Resolver:
+${try(module.private_dns.private_dns_resolver_name, "N/A")}
+
+Private DNS Forwarding Ruleset:
+${try(module.private_dns.private_dns_forwarding_ruleset_name, "N/A")}
+
+Private DNS Outbound Endpoint:
+${try(module.private_dns.private_dns_outbound_endpoint_name, "N/A")}
+
+TB
+}
+
+output "testbedconfig" {
+  description = "Azure Testbed results"
+  value       = local.testbedconfig
+}
+
+resource "local_file" "testbed" {
+  content  = local.testbedconfig
+  filename = "../testbed.txt"
+}
