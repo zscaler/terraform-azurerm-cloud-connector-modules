05f6c0d9e07e26834f6535660fc3acd82d83131c
Jameson Molnar <JMolnar@zscaler.com>
2024-05-03 16:48:36 -0400

    refactor: add workload/bastion instance selection for non-standard regions

--- modules/terraform-zscc-bastion-azure/main.tf
+++ modules/terraform-zscc-bastion-azure/main.tf
@@ -68,7 +68,7 @@ resource "azurerm_linux_virtual_machine" "bastion_vm" {
   location              = var.location
   resource_group_name   = var.resource_group
   network_interface_ids = [azurerm_network_interface.bastion_nic.id]
-  size                  = contains(local.unsupported_regions, lower(var.location)) ? "Standard_A3" : var.instance_size
+  size                  = local.instance_size_selection
   admin_username        = var.server_admin_username
   computer_name         = "${var.name_prefix}-bastion-${var.resource_tag}"
   admin_ssh_key {
--- modules/terraform-zscc-workload-azure/main.tf
+++ modules/terraform-zscc-workload-azure/main.tf
@@ -84,7 +84,7 @@ resource "azurerm_linux_virtual_machine" "workload_vm" {
   resource_group_name = var.resource_group
 
   network_interface_ids = [azurerm_network_interface.workload_nic[count.index].id]
-  size                  = contains(local.unsupported_regions, lower(var.location)) ? "Standard_A3" : var.instance_size
+  size                  = local.instance_size_selection
   admin_username        = var.server_admin_username
   computer_name         = "${var.name_prefix}-workload-${count.index + 1}-${var.resource_tag}"
   admin_ssh_key {
