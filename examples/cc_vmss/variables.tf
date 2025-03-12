27d379f7145d390c9e87cffc4d5e93c5ead2457d
Jameson Molnar <jmolnar@zscaler.com>
2025-03-12 11:05:12 -0400

    feat: add variable asp_sku_name for vmss deployments

--- examples/cc_vmss/variables.tf
+++ examples/cc_vmss/variables.tf
@@ -364,6 +364,21 @@ variable "path_to_scripts" {
   default     = ""
 }
 
+variable "asp_sku_name" {
+  type        = string
+  description = "SKU Name for the App Service Plan. Recommended Y1 (flex consumption) for function app unless not supported by Azure region"
+  default     = "Y1"
+  validation {
+    condition = (
+      var.asp_sku_name == "Y1" ||
+      var.asp_sku_name == "FC1" ||
+      var.asp_sku_name == "EP1" ||
+      var.asp_sku_name == "B1"
+    )
+    error_message = "Input asp_sku_name selected is not a valid/approved SKU Name."
+  }
+}
+
 # Azure Private DNS specific variables
 variable "zpa_enabled" {
   type        = bool
