51898982fd08cabc61558f6bc0ff3b0c3b210fa5
Srinivasa Prasad Karna <skarna@zscaler.com>
2026-07-21 00:55:34 +0530

    A new tag `LegacyVMNVA = "true"` is being injected into both the Cloud Connector VM and the Cloud Connector VMSS resources, merged alongside any existing `global_tags`. This tag is likely used to identify legacy VM NVA (Network Virtual Appliance) deployments for filtering, policy enforcement, or migration tracking purposes. (#75)

--- modules/terraform-zscc-ccvmss-azure/main.tf
+++ modules/terraform-zscc-ccvmss-azure/main.tf
@@ -88,7 +88,7 @@ resource "azurerm_orchestrated_virtual_machine_scale_set" "cc_vmss" {
 
   source_image_id = var.ccvm_source_image_id != null ? var.ccvm_source_image_id : null
 
-  tags = var.global_tags
+  tags = merge(var.global_tags, { LegacyVMNVA = "true" })
 
   depends_on = [
     var.backend_address_pool
