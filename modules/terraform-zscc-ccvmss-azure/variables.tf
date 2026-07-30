20746b0ec720864bffe81d6e859a4e4e5ba302f0
arkeshk <arkesh.kumar@zscaler.com>
2026-07-30 07:55:10 -0700

    Fix limits used by Azure VMSS (#77)

--- modules/terraform-zscc-ccvmss-azure/variables.tf
+++ modules/terraform-zscc-ccvmss-azure/variables.tf
@@ -181,18 +181,53 @@ variable "vmss_default_ccs" {
   type        = number
   description = "Default number of CCs in vmss."
   default     = 2
+
+  validation {
+    condition = (
+      var.vmss_default_ccs <= 16
+    )
+    error_message = "vmss_default_ccs cannot exceed 16 (hard limit of 16 Cloud Connectors per group)."
+  }
+
+  validation {
+    condition = (
+      var.vmss_default_ccs >= var.vmss_min_ccs && var.vmss_default_ccs <= var.vmss_max_ccs
+    )
+    error_message = "vmss_default_ccs must be between vmss_min_ccs and vmss_max_ccs (inclusive)."
+  }
 }
 
 variable "vmss_min_ccs" {
   type        = number
   description = "Minimum number of CCs in vmss."
   default     = 2
+
+  validation {
+    condition = (
+      var.vmss_min_ccs >= 1 && var.vmss_min_ccs <= 16
+    )
+    error_message = "vmss_min_ccs must be between 1 and 16."
+  }
 }
 
 variable "vmss_max_ccs" {
   type        = number
   description = "Maximum number of CCs in vmss."
-  default     = 16
+  default     = 4
+
+  validation {
+    condition = (
+      var.vmss_max_ccs <= 16
+    )
+    error_message = "vmss_max_ccs cannot exceed 16 (hard limit of 16 Cloud Connectors per group)."
+  }
+
+  validation {
+    condition = (
+      var.vmss_max_ccs >= var.vmss_min_ccs
+    )
+    error_message = "vmss_max_ccs must be greater than or equal to vmss_min_ccs."
+  }
 }
 
 variable "scale_out_evaluation_period" {
